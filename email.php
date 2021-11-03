20fba576dd34cd05218e73bd91c761c87176a563
jdcera4 <jdcera4@gmail.com>
2021-11-02 22:00:42 -0500

    emails

---
+++ email.php
@@ -0,0 +1,19 @@
+<?php
+$mail = $_POST['email'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje .= "Seras informado con la nueva informacion sobre nuestra aplicacion " " \r\n";
+$mensaje .= "Mantente antento sobre los nuevos cambios"" \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'ignaciobluuweb@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($email, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
